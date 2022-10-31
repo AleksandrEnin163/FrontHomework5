e2d5aca4a3f4eaa315dd38f814f89ba22e3b271e
alexenin163 <alexenin163@gmail.com>
2022-10-31 15:16:28 +0100

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,70 @@
+// Задача 3
+// for (let i = 0; i < 11; i++) {
+//     console.log(i)
+// }
+
+// Задача 4
+// for (let i = 0; i < 11; i += 2) {
+//     console.log(i)
+// }
+
+// Задача 5
+// for (let i = 5; i < 11; i++) {
+//     console.log(i)
+// }
+
+// Задача 6
+// for (let i = 10; i >= 0; i--) {
+//     console.log(i)
+// }
+
+// Задача 7
+// const n = 80;
+// const m = 40;
+
+// if (n > m) {
+//     for(let i = m; i <= n; i++){
+//         console.log(i)
+//     }
+// }else if (n < m) {
+//     for(let i = n; i <= m; i++){
+//         console.log(i)
+//     }
+// }
+
+// Задача 8
+// const n = 80;
+// const m = 40;
+
+// for (let i = m; i <= n; i++) {
+//     if(i % 2 == 0) {
+//         console.log(i)
+//     }
+// }
+
+// Задача 9
+// const n = 9;
+// const m = 5;
+// sum_chet = 0;
+// sum_nechet = 0;
+
+// for (let i = m; i <= n; i++) {
+//     if(i % 2 == 0) {
+//         sum_chet += i;
+//     } else if(i % 2 != 0) {
+//         sum_nechet += i;
+//     }
+// }
+
+// console.log(sum_chet);
+// console.log(sum_nechet);
+
+// Задача 10
+// const n = 10;
+// sum = 0;
+
+// for(let i = 1; i <= n; i++) {
+//     sum += i;
+// }
+
+// console.log(sum);
